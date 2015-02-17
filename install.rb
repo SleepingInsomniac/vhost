059499ac9fd2abcd3cbe6731b3120c81629347ef
Alex Clink <code@alexclink.com>
2015-02-16 22:05:48 -0500

    added --edit-conf flag. added install options

--- install.rb
+++ install.rb
@@ -1,10 +1,46 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
 require 'fileutils'
+require 'optparse'
 
 Dir.chdir File.expand_path(File.dirname(__FILE__))
 
-FileUtils.copy 'vhost', '/usr/local/bin/vhost'
-FileUtils.cp_r 'vhosts-conf', '/usr/local/etc/vhosts-conf'
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: ./install.rb [options]"
+  
+  opts.on "--prefix=PREFIX", "Custom prefix" do |p|
+    options[:prefix] = p
+  end
+  
+  opts.on "--config", "Just install configuration (implies --overwrite)" do |c|
+    options[:config] = c
+    options[:overwrite] = c
+  end
+  
+  opts.on "--overwrite", "Overwrite exsting config files" do |o|
+    options[:overwrite] = o
+  end
+  
+  opts.on "-h", "--help", "Help message" do
+    puts opts
+    exit
+  end
+  
+end.parse!
 
-File.chmod 0755, "/usr/local/bin/vhost"
+prefix = options[:prefix] || '/usr/local'
+
+unless options[:config]
+  FileUtils.copy 'vhost', File.join(prefix, 'bin/vhost')
+  File.chmod 0755, File.join(prefix, "bin/vhost")
+  puts "Installed vhost at #{prefix}"
+end
+
+FileUtils.rm_r File.join(prefix, 'etc/vhosts-conf') if options[:overwrite]
+
+unless File.exist? File.join(prefix, 'etc/vhosts-conf')
+  FileUtils.cp_r 'vhosts-conf', File.join(prefix, 'etc/vhosts-conf')
+  puts "Installed configuration at #{prefix}"
+end
