d2c5392fd37cb49c3b12155ae4e3d57065aaea32
Alex Clink <code@alexclink.com>
2015-02-16 21:41:48 -0500

    typo -_-

--- install.rb
+++ install.rb
@@ -7,4 +7,4 @@ Dir.chdir File.expand_path(File.dirname(__FILE__))
 FileUtils.copy 'vhost', '/usr/local/bin/vhost'
 FileUtils.cp_r 'vhosts-conf', '/usr/local/etc/vhosts-conf'
 
-File.chmod 0755, "/usr/local/bin/vhosts"
+File.chmod 0755, "/usr/local/bin/vhost"
