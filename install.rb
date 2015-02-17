33f4d4c0f2d56fc2dd405e403343f6d5dba7abab
Alex Clink <code@alexclink.com>
2015-02-16 21:40:16 -0500

    fixed install script

--- install.rb
+++ install.rb
@@ -5,6 +5,6 @@ require 'fileutils'
 Dir.chdir File.expand_path(File.dirname(__FILE__))
 
 FileUtils.copy 'vhost', '/usr/local/bin/vhost'
-FileUtils.copy 'vhosts-conf', '/usr/local/etc/vhosts-conf'
+FileUtils.cp_r 'vhosts-conf', '/usr/local/etc/vhosts-conf'
 
 File.chmod 0755, "/usr/local/bin/vhosts"
